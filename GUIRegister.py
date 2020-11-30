4a5b7c080fd6a5677dd5b1f16ee32ed616d86cb8
noorwa1 <75200572+noorwa1@users.noreply.github.com>
2020-11-30 23:29:47 +0200

    Update GUIRegister.py

--- GUIRegister.py
+++ GUIRegister.py
@@ -1,7 +1,7 @@
 import kivy
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
+from kivy.app import App 
+from kivy.uix.label import Label 
+from kivy.uix.gridlayout import GridLayout 
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
