2f4c82f7e42fb8bf6aadbacb4a560ec257a5bdf5
Abdalbaset10 <Abdalbaset1998@gmail.com>
2020-11-30 11:34:29 +0200

    USER INTERFACE

--- GUIRegister.py
+++ GUIRegister.py
@@ -7,6 +7,21 @@ from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.config import Config
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager,Screen
+
+class HomeWindo(Screen):
+    pass
+
+class RegiWindo(Screen):
+    pass
+
+class Loginwindo(Screen):
+    pass
+
+class WindowManger(ScreenManager):
+    pass
+kv=Builder.load_file("mainapp.kv")
 
 
 
@@ -15,22 +30,16 @@ from kivy.uix.relativelayout import RelativeLayout
 
 
 
-class MyApp(App):
+
+
+class MyMainApp(App):
     def build(self):
 
-        r1 = RelativeLayout(size=(10, 10))
-        t1 = Label(text="Welcome to School App",pos_hint ={'center_x':.5, 'center_y':.8},size_hint=(1, 1))
-        b1 = Button(size_hint=(.1,.1),pos_hint ={'center_x':.9, 'center_y':.6},text="Register")
-        b2 = Button(size_hint=(.1, .1), pos_hint={'center_x': .9, 'center_y': .4}, text="Login")
-        r1.add_widget(t1)
-        r1.add_widget(b1)
-        r1.add_widget(b2)
-        return r1
-
+       return kv
 
 
 
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    MyMainApp().run()
