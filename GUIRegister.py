57e25be2afebda87225def8f5b67bb6c77a1d4b9
Abdalbaset10 <Abdalbaset1998@gmail.com>
2020-11-30 13:38:43 +0200

    USER INTERFACE

--- GUIRegister.py
+++ GUIRegister.py
@@ -9,19 +9,49 @@ from kivy.config import Config
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager,Screen
+from kivy.uix.popup import Popup
+from kivy.properties import ObjectProperty
+
+
+
 
 class HomeWindo(Screen):
     pass
 
 class RegiWindo(Screen):
-    pass
+    idn = ObjectProperty(None)
+    fullname = ObjectProperty(None)
+    dob = ObjectProperty(None)
+    username = ObjectProperty(None)
+    password = ObjectProperty(None)
+
+    def regibtn(self):
+        if self.idn.text !='' and self.fullname.text !='' and self.dob.text !='' and self.username.text !='' and self.password.text !='':
+            print("Done")
+        else:
+            print("HEY")
+            invalidForm()
+
+
+
+
 
 class Loginwindo(Screen):
+    pass
+
+
+class MangerLog(Screen):
     pass
 
 class WindowManger(ScreenManager):
     pass
 kv=Builder.load_file("mainapp.kv")
+WM=WindowManger()
+
+
+def invalidForm():
+    pop = Popup(title='Invalid Form', content=Label(text='Please fill in all inputs with valid information.'),size_hint=(None, None), size=(400, 400))
+    pop.open()
 
 
 
